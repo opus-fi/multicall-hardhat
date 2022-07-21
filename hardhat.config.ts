0e05adc777798872e9537ed37036a88dbc2c5eb1
0xPeregrinus <0xPeregrinus@proton.me>
2022-07-21 18:22:34 +0700

    Multicall contract is ready to be deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.15',
+  networks: {
+    neonlabs: {
+      url: process.env.PROXY_URL ? process.env.PROXY_URL : undefined,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : undefined,
+      chainId: process.env.NETWORK_ID
+        ? Number(process.env.NETWORK_ID)
+        : undefined,
+      gas: 3000000,
+      gasPrice: 200000000000,
+      blockGasLimit: 10000000,
+      allowUnlimitedContractSize: false,
+      timeout: 1000000,
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,15 @@
-import { ethers } from "hardhat";
+import * as dotenv from 'dotenv';
+import { ethers } from 'hardhat';
+
+dotenv.config();
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const Multicall = await ethers.getContractFactory('Multicall');
+  const multicall = await Multicall.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await multicall.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log('Multicall deployed to: %s', multicall.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
